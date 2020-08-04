afde4b5726204e8f6f30edd7aed3dd86b81986dd
malak <malakeltanekhey@gmail.com>
2020-08-04 20:32:13 +0200

    SignIn Screen

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget appBarMain() {
+  return AppBar(
+    title: Image.asset(
+      'assets/images/pepol.jpg',
+      height: 50,
+    ),
+  );
+}
+
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.white54),
+      focusedBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+      enabledBorder:
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(color: Colors.white, fontSize: 16);
+}
+
+TextStyle mediumTextStyle() {
+  return TextStyle(color: Colors.white, fontSize: 17);
+}
